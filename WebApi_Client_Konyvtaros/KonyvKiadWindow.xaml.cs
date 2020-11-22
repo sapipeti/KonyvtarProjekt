a174bc2f5f81c49f15cc44e7bc383cd6c941b3ca
Istvan Molnar <v1r7u3l@users.noreply.github.com>
2020-11-22 16:45:05 +0100

    Könyvtáros kliens: KonyvKiadWindow létrehozva, dizájnnal, még funkcionalitás nélkül

---
+++ WebApi_Client_Konyvtaros/KonyvKiadWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WebApi_Common.DataProviders;
+
+namespace WebApi_Client_Konyvtaros
+{
+    /// <summary>
+    /// Interaction logic for KonyvKiadWindow.xaml
+    /// </summary>
+    public partial class KonyvKiadWindow : Window
+    {
+        public KonyvKiadWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Megsem_Click(object sender, RoutedEventArgs e)
+        {
+            SplashWindow sw = new SplashWindow();
+            sw.Show();
+            this.Close();
+        }
+
+        private void Kiadas_Click(object sender, RoutedEventArgs e)
+        {
+            if(darabszamTextBox.Text.Equals("") || !datePicker.SelectedDate.HasValue || neptunkodTextBox.Text.Equals(""))
+            {
+                MessageBox.Show("Kérlek tölts ki minden mezőt!", "Hiba");
+            }
+            else
+            {
+                
+                SplashWindow sw = new SplashWindow();
+                sw.Show();
+                this.Close();
+            }
+        }
+    }
+}
