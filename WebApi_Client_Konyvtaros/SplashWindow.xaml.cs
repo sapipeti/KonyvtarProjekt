9da8919ef65235ec7f115e7daf9aa706e59fc39a
Istvan Molnar <v1r7u3l@users.noreply.github.com>
2020-11-22 16:40:42 +0100

    Könyvtáros kliens splash window létrehozva, közel végleges UI dizájnnal

---
+++ WebApi_Client_Konyvtaros/SplashWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WebApi_Common.DataProviders;
+using WebApi_Common.Models;
+
+namespace WebApi_Client_Konyvtaros
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KonyvKeresButton_Click(object sender, RoutedEventArgs e)
+        {
+            KonyvKiadWindow kkeresw = new KonyvKiadWindow();
+            kkeresw.Show();
+            this.Close();
+        }
+
+        private void KonyvKiadButton_Click(object sender, RoutedEventArgs e)
+        {
+            KonyvKiadWindow kkiadw = new KonyvKiadWindow();
+            kkiadw.Show();
+            this.Close();
+        }
+
+
+    }
+}
